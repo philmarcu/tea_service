b877d33beecf85b0692de1a01cd8a07833e98075
philmarcu <philmark18@gmail.com>
2022-11-02 14:09:47 -0600

    Add serializers for subscriptions + customers, Add subscriptions controller + customers controller

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::CustomersController < ApplicationController
+  def subscriptions
+  end
+end
--- app/controllers/concerns/serializer.rb
+++ app/controllers/concerns/serializer.rb
@@ -2,4 +2,8 @@ module Serializer
   def tea_serializer(tea)
     TeaSerializer.new(tea)
   end
+
+  def sub_serializer(sub)
+    SubscriptionSerializer.new(sub)
+  end
 end
