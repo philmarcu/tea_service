6d0d8fb151dacc8275b9a72a6ae2d55946f842bb
philmarcu <philmark18@gmail.com>
2022-11-02 17:59:52 -0600

    Index: add test + action for finding all of a customers subscriptions active or cancelled

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CustomersController < ApplicationController
   def subscriptions
+    customer = Customer.find(params[:customer_id])
+    json_response(sub_serializer(customer.subscriptions))
   end
 end
---
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Subscriptions API' do
+  context 'happy path' do
+    let!(:billy) {create(:customer)}
+    let!(:mark) {create(:customer)}
+    let(:teas) {create_list(:tea, 3)}
+    let!(:sub1) {create(:subscription, customer_id: billy.id, tea_id: teas[0].id)}
+    let!(:sub2) {create(:subscription, customer_id: billy.id, tea_id: teas[1].id)}
+    let!(:sub3) {create(:subscription, customer_id: billy.id, tea_id: teas[2].id)}
+
+    it 'gets all of a Customer`s subscriptions' do
+      get api_v1_customer_subscriptions_path(billy.id)
+
+      parsed_json = JSON.parse(response.body, symbolize_names: true)
+      results = parsed_json[:data]
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      expect(results.size).to eq(3)
+      results.each do |sub|
+        attributes = sub[:attributes]
+        expect(sub).to have_key(:id)
+        expect(sub[:type]).to eq("subscriptions")
+        expect(attributes[:title]).to be_a String
+        expect(attributes[:price]).to be_a Float
+        expect(attributes[:status]).to be_a String
+        expect(attributes[:frequency]).to be_a String
+      end
+    end
+  end
+end
