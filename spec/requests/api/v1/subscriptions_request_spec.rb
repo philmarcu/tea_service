a251bc23bda105df0f414ddad78c36334f20cde0
philmarcu <philmark18@gmail.com>
2022-11-02 18:02:38 -0600

    Refactor: change last sub to account for active + cancelled subscriptions

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -7,18 +7,20 @@ RSpec.describe 'Subscriptions API' do
     let(:teas) {create_list(:tea, 3)}
     let!(:sub1) {create(:subscription, customer_id: billy.id, tea_id: teas[0].id)}
     let!(:sub2) {create(:subscription, customer_id: billy.id, tea_id: teas[1].id)}
-    let!(:sub3) {create(:subscription, customer_id: billy.id, tea_id: teas[2].id)}
+    let!(:sub3) {create(:subscription, customer_id: billy.id, tea_id: teas[2].id, status: "cancelled")}
 
     it 'gets all of a Customer`s subscriptions' do
       get api_v1_customer_subscriptions_path(billy.id)
 
       parsed_json = JSON.parse(response.body, symbolize_names: true)
       results = parsed_json[:data]
+      last_sub_status = results.last[:attributes][:status]
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
 
       expect(results.size).to eq(3)
+      expect(last_sub_status).to eq("cancelled")
       results.each do |sub|
         attributes = sub[:attributes]
         expect(sub).to have_key(:id)
